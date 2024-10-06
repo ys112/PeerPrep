c526b075c639e209834f75f50e8607bd8893d28b
ys112 <yusian@u.nus.edu>
2024-10-06 21:08:08 +0800

    Add admin check to question route

--- frontend/src/routes/_authenticated/questions.tsx
+++ frontend/src/routes/_authenticated/questions.tsx
@@ -1,6 +1,21 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { QuestionsPage } from "../../components/Questions/QuestionsPage";
+import { userStorage } from "../../utils/userStorage";
+import { notifications } from "@mantine/notifications";
 
 export const Route = createFileRoute("/_authenticated/questions")({
   component: QuestionsPage,
+  beforeLoad: () => {
+    const user = userStorage.getUser();
+    if (!user) {
+      throw redirect({ to: "/login", throw: true });
+    } else if (!user?.isAdmin) {
+      notifications.show({
+        title: "Unauthorized",
+        message: "You are not authorized to view this page",
+        color: "red",
+      });
+      throw redirect({ to: "/", throw: true });
+    }
+  },
 });
