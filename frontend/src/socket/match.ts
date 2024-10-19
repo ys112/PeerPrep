d9ec92e338c5984f4d41607a51796092d81b840a
donljh <don.limjunhong@gmail.com>
2024-10-19 22:32:53 +0800

    fix socket auth issue

--- frontend/src/socket/match.ts
+++ frontend/src/socket/match.ts
@@ -1,9 +1,9 @@
 import { io, Socket } from "socket.io-client";
 import { accessTokenStorage } from "../utils/accessTokenStorage";
 
-const token = accessTokenStorage.getAccessToken();
 
 export function initializeMatchSocket() {
+  const token = accessTokenStorage.getAccessToken();
   return io(import.meta.env.VITE_MATCH_SERVICE_API_URL, {
     auth: {
       token,
