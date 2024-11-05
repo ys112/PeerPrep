27d3b86e1bb6c8ac2045022a9fda1afd962aaff4
Cloud <44526554+Cloud7050@users.noreply.github.com>
2024-11-05 19:59:18 +0800

    fix: Update error for JWT_SECRET_PATH

--- services/user/src/utils/jwt-secret.ts
+++ services/user/src/utils/jwt-secret.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 function getJwtSecret(): string {
   if (process.env.JWT_SECRET_PATH === undefined) {
-    throw new Error('JWT_SECRET and JWT_SECRET_PATH is not set')
+    throw new Error('JWT_SECRET_PATH is not set')
   }
 
   return fs.readFileSync(process.env.JWT_SECRET_PATH!, 'utf8')
