51ec6d71665ff309fa46edf4903e06269619493c
Cloud <44526554+Cloud7050@users.noreply.github.com>
2024-11-07 22:09:40 +0800

    feat(fe): Responsive, revert again

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,22 +1,20 @@
 import { User } from '@common/shared-types/dist';
-import { Divider, Loader, Paper, Stack, Title } from "@mantine/core";
+import { Divider, Group, Loader, Paper, Stack, Title } from "@mantine/core";
 import { userStorage } from "../../utils/userStorage";
 
 export function Dashboard() {
   let user: User = userStorage.getUser()!;
 
-  return <>
-		<Paper mb='md' p='md' withBorder radius='md' shadow='md'>
-			<Stack ta='center'>
-				<Title order={2}>Welcome, {user.username}</Title>
-			</Stack>
-		</Paper>
-		<Paper mb='md' p='md' withBorder radius='md' shadow='md'>
-			<Stack>
-				<Title order={3}>History</Title>
-				<Divider></Divider>
-				<Loader mx='auto' color='lime' />
-			</Stack>
-		</Paper>
-	</>;
+  return <Stack>
+		<Title order={1} ta='center' >Welcome, {user.username}</Title>
+		<Group>
+			<Paper w={{ base: '100%', lg: '50%' }} mx='auto' p='md' withBorder radius='md' shadow='md'>
+				<Stack>
+					<Title order={2}>History</Title>
+					<Divider></Divider>
+					<Loader mx='auto' color='lime' />
+				</Stack>
+			</Paper>
+		</Group>
+	</Stack>;
 }
