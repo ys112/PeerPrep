723c7a85bf459509bd6f87f47202206c430359fd
asaierika <anyaasai@gmail.com>
2024-11-08 03:58:12 +0800

    Add syntax hilight and autocomplete

--- frontend/src/components/Collaboration/CodingEditor.tsx
+++ frontend/src/components/Collaboration/CodingEditor.tsx
@@ -4,8 +4,7 @@ import * as Y from "yjs";
 import { yCollab } from "y-codemirror.next";
 
 import { EditorView, basicSetup } from "codemirror";
-import { keymap, lineNumbers } from "@codemirror/view";
-import { defaultKeymap } from "@codemirror/commands";
+import { autocompletion } from "@codemirror/autocomplete";
 import { EditorState } from "@codemirror/state";
 import { javascript } from "@codemirror/lang-javascript";
 import { useEffect, useRef } from "react";
@@ -68,12 +67,11 @@ export default function CodingEditor({ roomId, isOpen, onCodeChange }: Props) {
     const state = EditorState.create({
       doc: yText.toString(),
       extensions: [
-        //TODO add more languages and do basic setup
-        [keymap.of(defaultKeymap)],
-        lineNumbers(),
+        basicSetup,
+        autocompletion(),
         javascript(),
         fixedHeightEditor,
-        EditorState.readOnly.of(!isOpen), // Disable editing when room closed
+        EditorState.readOnly.of(!isOpen),
         yCollab(yText, provider.awareness, { undoManager }),
       ],
     });
