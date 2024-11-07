83c5bb8496fd4e9ccce30abd82e5862ee70fd8fa
Cloud <44526554+Cloud7050@users.noreply.github.com>
2024-11-07 21:45:55 +0800

    feat: Revert & tweak design

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,17 +1,22 @@
+import { User } from '@common/shared-types/dist';
 import { Divider, Loader, Paper, Stack, Title } from "@mantine/core";
 import { userStorage } from "../../utils/userStorage";
 
 export function Dashboard() {
-  const user = userStorage.getUser()!;
+  let user: User = userStorage.getUser()!;
 
   return <>
-    <Paper mb='md' p='md' withBorder radius='md' shadow='md'>
-      <Stack ta='center'>
-        <Title order={2}>Welcome, {user.username}</Title>
-        <Divider my="md" />
-        <Title order={3}>Question History</Title>
-        <Loader mx='auto' color='lime' />
-      </Stack>
-    </Paper>
-  </>;
+		<Paper mb='md' p='md' withBorder radius='md' shadow='md'>
+			<Stack ta='center'>
+				<Title order={2}>Welcome, {user.username}</Title>
+			</Stack>
+		</Paper>
+		<Paper mb='md' p='md' withBorder radius='md' shadow='md'>
+			<Stack>
+				<Title order={3}>History</Title>
+				<Divider></Divider>
+				<Loader mx='auto' color='lime' />
+			</Stack>
+		</Paper>
+	</>;
 }
