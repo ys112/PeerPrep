bd4d93f9982643ac2091ef6114383449746e89c7
Cloud <44526554+Cloud7050@users.noreply.github.com>
2024-11-08 14:27:07 +0800

    feat: Pretty sorted timestamp, refactor nesting

--- frontend/src/components/Dashboard/AttemptsTable.tsx
+++ frontend/src/components/Dashboard/AttemptsTable.tsx
@@ -1,5 +1,6 @@
 import { Attempt, ExtractedUser, Question } from '@common/shared-types';
 import { Loader, Table, TableData, Text } from '@mantine/core';
+import { DateTime } from 'luxon';
 import { useEffect, useState } from 'react';
 import { questionClient } from '../../api/questions';
 import { roomClient } from '../../api/room';
@@ -8,47 +9,56 @@ interface Props {
   user: ExtractedUser;
 }
 
+function makeTableData(attempts: Attempt[] | null, questions: Question[] | null): TableData | null {
+  if (attempts === null || questions === null) return null;
+
+  return {
+    body: attemptsToRows(attempts, questions),
+  };
+}
+function attemptsToRows(attempts: Attempt[], questions: Question[]): string[][] {
+  // Sort descending
+  let sortedAttempts: Attempt[] = attempts!.sort((a, b) => b.createdAt - a.createdAt);
+  return sortedAttempts.map((attempt) => {
+    let questionTitle: string = questionIdToTitle(attempt.questionId, questions);
+    let prettyTimestamp: string = epochToTimestamp(attempt.createdAt);
+    return [questionTitle, prettyTimestamp];
+  })
+}
+function questionIdToTitle(id: string, questions: Question[]): string {
+  let matchingQuestions = questions!.filter((question) => question.id === id);
+
+  if (matchingQuestions.length < 1) return "Unknown Question";
+
+  return matchingQuestions[0]!.title;
+}
+function epochToTimestamp(epoch?: number): string {
+  if (epoch === undefined) return "(No Timestamp)";
+
+  let luxonDate: DateTime = DateTime.fromMillis(epoch);
+  // https://moment.github.io/luxon/#/formatting
+  return `${luxonDate.toFormat("d MMM")} '${luxonDate.toFormat("kk, h:mma").toLowerCase()}`;
+}
+
 export function AttemptsTable(props: Props) {
   let [attempts, setAttempts] = useState<Attempt[] | null>(null);
   let [questions, setQuestions] = useState<Question[] | null>(null);
 
-  let tableData: TableData | null = makeTableData()
-  function makeTableData(): TableData | null {
-    if (attempts === null || questions === null) return null;
-
-    return {
-      body: attemptsToRows(),
-    };
-
-    function attemptsToRows(): string[][] {
-      function questionIdToTitle(id: string) {
-        let matchingQuestions = questions!.filter((question) => question.id === id);
-
-        if (matchingQuestions.length < 1) return "Unknown Question"
-
-        return matchingQuestions[0]!.title;
-      }
-
-      return attempts!.map(attempt => {
-        return [questionIdToTitle(attempt.questionId)]
-      })
-    }
-  }
+  let tableData: TableData | null = makeTableData(attempts, questions);
 
   useEffect(() => {
     roomClient.getAttempts(props.user.id)
       .then((fetchedAttempts: Attempt[]) => {
-        // Reverse sort, docs are presumably appended
-        setAttempts(fetchedAttempts.reverse())
+        setAttempts(fetchedAttempts)
         console.log(fetchedAttempts)
-      })
+      });
 
     questionClient.getQuestions()
       .then((fetchedQuestions: Question[]) => {
         setQuestions(fetchedQuestions)
         console.log(fetchedQuestions)
-      })
-  }, [props.user])
+      });
+  }, [props.user]);
 
   // [UI]
   if (tableData === null) {
@@ -56,7 +66,7 @@ export function AttemptsTable(props: Props) {
   }
 
   if (tableData.body!.length <= 0) {
-    return <Text>You have not collaborated with others yet.</Text>
+    return <Text>You have not collaborated with others yet.</Text>;
   }
 
   return <Table data={tableData} />;
